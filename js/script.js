4035aa00b15fc11cce7ce173bec32bdccd37e3fc
rishabhdv1 <rishabh.bairagi.136@gmail.com>
2023-09-06 04:14:20 -0700

    new

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+//1. Function defination is a one time process
+function getmyname(){ // This is not a per the camelCase standard
+    console.log("Rishabh");
+}
+
+function getMySurname(){ // This fnction name is as per the camelCase standard
+    // Function body
+    //inside the function body i can write many sstatements
+    console.log("Vaishnav"); // Statement = Instructions
+
+}
+
+//2. Function calling/invoking call/invoke
+
+getmyname();
+getMySurname();
---
+++ js/script2.js
@@ -0,0 +1,15 @@
+//Lets create a function which can add  numbers
+
+//1. Function defination (One Time)
+
+function addTwoNumbers(n1,n2){
+    
+    //every function return something
+    return n1+n2;
+}
+
+//2.  Function calling (Many Time)
+    //Associativity Principle
+    // Left = Right
+var result = addTwoNumbers(10,20);
+console.log(result);
